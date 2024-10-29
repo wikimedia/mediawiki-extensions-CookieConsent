7177e90477f2b73a39de713e2f3736e26a2e6e94
Marijn van Wezel <marijnvanwezel@gmail.com>
2024-10-29 10:49:21 +0100

    Add support for iframes and allow multiple categories

--- resources/ext.CookieConsent/ext.CookieConsent.init.js
+++ resources/ext.CookieConsent/ext.CookieConsent.init.js
@@ -18,6 +18,33 @@
 		parent.removeChild(script);
 	}
 
+	function enableIframe( iframe ) {
+		const src = iframe.getAttribute( 'data-src' );
+		iframe.setAttribute( 'src', src );
+	}
+
+	function enableScripts() {
+		const scripts = document.querySelectorAll( 'script[data-cookieconsent]' );
+		for ( const script of scripts ) {
+			const categories = script.getAttribute( 'data-cookieconsent' ).split(',');
+			const consents = categories.map((category) => cookieConsent.isConsentGiven( category ) );
+			if ( consents.some( ( p ) => p ) ) {
+				enableScript( script );
+			}
+		}
+	}
+
+	function enableIframes() {
+		const iframes = document.querySelectorAll( 'iframe[data-cookieconsent]' );
+		for ( const iframe of iframes ) {
+			const categories = iframe.getAttribute( 'data-cookieconsent' ).split(',');
+			const consents = categories.map((category) => cookieConsent.isConsentGiven( category ) );
+			if ( consents.some( ( p ) => p ) ) {
+				enableIframe( iframe );
+			}
+		}
+	}
+
 	$.when( $.ready ).then( function () {
 		// Add a click handler for managing cookie preferences to #manage-cookie-preferences.
 		$("#manage-cookie-preferences").click(function (e) {
@@ -27,14 +54,9 @@
 		// If the dialog has not been dismissed, show it immediately.
 		if ( !cookieConsent.isDismissed() ) {
 			cookieConsent.openSimpleDialog();
-		}
-
-		const scripts = document.querySelectorAll( 'script[data-cookieconsent]' );
-		for ( const script of scripts ) {
-			const cookieCategoryName = script.getAttribute( 'data-cookieconsent' );
-			if ( cookieConsent.isConsentGiven( cookieCategoryName ) ) {
-				enableScript( script );
-			}
+		} else {
+			enableScripts();
+			enableIframes();
 		}
 	} );
 } )( jQuery, cookieConsent );
