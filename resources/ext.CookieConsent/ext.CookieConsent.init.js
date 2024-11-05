657aae2400639558cbecd0cf8d8dae265cf7c383
Marijn van Wezel <marijnvanwezel@gmail.com>
2024-11-05 12:25:39 +0100

    Add event that fires after tags have been processed
    Task: T379058
    Change-Id: I10efc60be24147490f6013511f6850e4c14f8758

--- resources/ext.CookieConsent/ext.CookieConsent.init.js
+++ resources/ext.CookieConsent/ext.CookieConsent.init.js
@@ -3,60 +3,7 @@
  */
 
 ( function ( $, cookieConsent ) {
-	function enableScript( script ) {
-		const newScript = document.createElement('script');
-		newScript.text = script.text;
-		for ( const attributeName of script.getAttributeNames() ) {
-			newScript.setAttribute( attributeName, script.getAttribute( attributeName ) );
-		}
-
-		newScript.setAttribute('type', 'text/javascript');
-
-		const parent = script.parentElement;
-
-		parent.insertBefore(newScript, script);
-		parent.removeChild(script);
-	}
-
-	function enableIframe( iframe ) {
-		const src = iframe.getAttribute( 'data-src' );
-		iframe.setAttribute( 'src', src );
-	}
-
-	function enableScripts() {
-		const scripts = document.querySelectorAll( 'script[data-cookieconsent]' );
-		for ( const script of scripts ) {
-			const categories = script.getAttribute( 'data-cookieconsent' ).split(',');
-			const consents = categories.map((category) => cookieConsent.isConsentGiven( category ) );
-			if ( consents.some( ( p ) => p ) ) {
-				enableScript( script );
-			}
-		}
-	}
-
-	function enableIframes() {
-		const iframes = document.querySelectorAll( 'iframe[data-cookieconsent]' );
-		for ( const iframe of iframes ) {
-			const categories = iframe.getAttribute( 'data-cookieconsent' ).split(',');
-			const consents = categories.map((category) => cookieConsent.isConsentGiven( category ) );
-			if ( consents.some( ( p ) => p ) ) {
-				enableIframe( iframe );
-			}
-		}
-	}
-
 	$.when( $.ready ).then( function () {
-		// Add a click handler for managing cookie preferences to #manage-cookie-preferences.
-		$("#manage-cookie-preferences").click(function (e) {
-			cookieConsent.openDetailedDialog();
-		});
-
-		// If the dialog has not been dismissed, show it immediately.
-		if ( !cookieConsent.isDismissed() ) {
-			cookieConsent.openSimpleDialog();
-		} else {
-			enableScripts();
-			enableIframes();
-		}
+		cookieConsent.init();
 	} );
 } )( jQuery, cookieConsent );
